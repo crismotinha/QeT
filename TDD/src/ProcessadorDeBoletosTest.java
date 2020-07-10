a354a2d6ca5f03e23658cc6d08f409267332d3d5
crismotinha <cmmartins@stone.com.br>
2020-07-10 17:48:59 -0300

    Adiciona teste para valor de soma dos boletos igual ao da fatura não passando

--- TDD/src/ProcessadorDeBoletosTest.java
+++ TDD/src/ProcessadorDeBoletosTest.java
@@ -29,6 +29,28 @@ public class ProcessadorDeBoletosTest {
     }
 
     @Test
+    public void PagarFatura_QuandoOValorDosBoletosForIgualQueOValorTotal_DeveMarcarComoPaga(){
+        //Arrange
+        double valorFatura = 100;
+        double valorBoleto = 50;
+
+        Boleto boleto1 = new Boleto(UUID.randomUUID(), new Date(), valorBoleto);
+        Boleto boleto2 = new Boleto(UUID.randomUUID(), new Date(), valorBoleto);
+
+        Fatura fatura = new Fatura(new Date(), valorFatura, "Teste", new Boleto[]{boleto1, boleto2},false);
+
+        boolean esperado = true;
+
+        //Act
+        ProcessadorDeBoletos processadorDeBoletos = new ProcessadorDeBoletos();
+        processadorDeBoletos.PagarFatura(fatura);
+
+        //Assert
+        Assert.assertEquals(esperado, fatura.isPaga());
+
+    }
+
+    @Test
     public void PagarFatura_QuandoOValorDosBoletosForMenorQueOValorTotal_DeveMarcarComoNaoPaga(){
         //Arrange
         double valorFatura = 100;
