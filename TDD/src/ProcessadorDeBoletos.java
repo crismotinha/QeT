449e44c8655807fbb6873a016e65215341ade895
crismotinha <cmmartins@stone.com.br>
2020-07-10 17:48:03 -0300

    Adiciona lógica em PagarFatura e faz teste passar

--- TDD/src/ProcessadorDeBoletos.java
+++ TDD/src/ProcessadorDeBoletos.java
@@ -1,5 +1,15 @@
 public class ProcessadorDeBoletos {
     public void PagarFatura(Fatura fatura){
-        fatura.setPaga(true);
+        double somaBoletos = 0;
+
+        Boleto[] boletos = fatura.getBoletos();
+
+        for (Boleto boleto : boletos) {
+            somaBoletos += boleto.getValorPago();;
+        }
+
+        if (somaBoletos > fatura.getValorTotal()) {
+            fatura.setPaga(true);
+        }
     };
 }
